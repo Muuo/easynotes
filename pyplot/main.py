4471382cbe63b95b168146a6e3faf08cd3e251a8
Muuo Wambua <muuowambua@yahoo.com>
2012-09-27 10:33:13 +0300

    python script can now dplay the data

--- pyplot/main.py
+++ pyplot/main.py
@@ -1,1 +1,28 @@
-f=open("../recorded.raw",'rb');
+import pyaudio
+import wave
+
+chunk = 1024
+
+wf = wave.open('recorded.wav', 'rb')
+
+p = pyaudio.PyAudio()
+
+# open stream
+stream = p.open(format =
+                p.get_format_from_width(wf.getsampwidth()),
+                channels = wf.getnchannels(),
+                rate = wf.getframerate(),
+                output = True)
+print str(wf.getnchannels())+" channels"
+print str(wf.getframerate())+" rate"
+
+# read data
+data = wf.readframes(chunk)
+
+# play stream
+while data != '':
+    stream.write(data)
+    data = wf.readframes(chunk)
+
+stream.close()
+p.terminate()
