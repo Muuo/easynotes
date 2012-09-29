3de24601e7d7c401077ebbe1300ead87c2545f58
Muuo Wambua <muuowambua@yahoo.com>
2012-09-29 03:51:37 +0300

    added another script to analyze the mono track

---
+++ pyplot/analyze.py
@@ -0,0 +1,25 @@
+import pyaudio
+import wave
+import numpy as np
+from matplotlib.pyplot import *
+import array
+
+#chunk = 512*3
+
+wf = wave.open('recorded.wav', 'rb')
+
+print str(wf.getsampwidth()*8)+" bit"
+print str(wf.getnchannels())+" channels"
+print str(wf.getframerate())+" rate"
+print str(wf.getnframes())+" frames"
+
+# read data
+data = wf.readframes(wf.getnframes())
+data = array.array('i',data)
+
+out=np.fft.fft(data)
+#plot(abs(out))
+show()
+plot(data)
+show()
+
